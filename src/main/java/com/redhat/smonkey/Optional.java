d7efbf22495876e6b42bb60bb5f45f7587f9dbce
Burak Serdar <bserdar@redhat.com>
2014-04-16 09:29:07 -0600

    Add invoker script, add rpm

--- src/main/java/com/redhat/smonkey/Optional.java
+++ src/main/java/com/redhat/smonkey/Optional.java
@@ -26,7 +26,7 @@ public class Optional implements Generator {
     @Override
     public String describe() {
         return 
-            " { \"optional\": { \"percent\": <decimal between 0 and 1>, \"value\": {object} } }\n"+
+            " { \"optional\": { \"percent\": <decimal between 0 and 100>, \"value\": {object} } }\n"+
             "   Optionally generates a field value. The field exists with the value described in 'value'\n"+
             "   with probability 'percent'. By default, 'percent' is 0.5.";
     }
@@ -38,7 +38,7 @@ public class Optional implements Generator {
 
     @Override
      public JsonNode generate(JsonNodeFactory nodeFactory,JsonNode data,Monkey mon) {
-        double percent=Utils.asDouble(data.get("percent"), .5)*100.0;
+        double percent=Utils.asDouble(data.get("percent"), 50.0);
         if(Utils.rndBool((int)percent)) {
             JsonNode value=data.get("value");
             if(value==null)
