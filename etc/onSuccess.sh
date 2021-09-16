dd70296a300eef6377f92b9dbc1d8caddbca2593
Derek Haynes <dhaynes@redhat.com>
2021-09-16 10:02:22 +0200

    Remove TravisCI config

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -2,4 +2,4 @@
 
 echo "DEPLOY MASTER TRAVIS BUILD"
 echo "Current directory is $(pwd)"
-'[[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_JDK_VERSION == "openjdk7" ]] && { mvn clean deploy -DskipTests; };'
+'[[ $BRANCH == "master" ]] && [[ $JDK_VERSION == "openjdk7" ]] && { mvn clean deploy -DskipTests; };'
